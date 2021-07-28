3f992cfcaf66125473a8d5fc5a727580393517cf
amnhh <2570079254@qq.com>
2021-07-28 08:48:00 +0800

    chore: quotes

--- lib/hook/__test__/SyncHook.spec.ts
+++ lib/hook/__test__/SyncHook.spec.ts
@@ -1,10 +1,8 @@
 import SyncHook from '../SyncHook'
 
-describe('SyncHook', function() {
+describe('SyncHook', function () {
   it('should throw without a valid name', () => {
     const hook = new SyncHook()
-    expect(() => hook.tap("", () => {})).toThrow(
-      new Error("Missing name for tap")
-    );
+    expect(() => hook.tap('', () => {})).toThrow()
   })
 })
